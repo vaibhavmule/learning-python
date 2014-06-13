743bf4f35d383c9c9e800a4b676cb37a4cd5e92c
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-13 18:07:06 +0530

    added learn the python hard way exercise 9

---
+++ learn-python-hard-way-exercies/ex9.py
@@ -0,0 +1,23 @@
+#This is Exericse 9: Printing, Printing, Printing
+
+# Here's some new strange stuff, remember type it exactly.
+
+# A variable storing 7 days of week
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# A variable storing months till August
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Printing all days with sentence
+print "Here are the days: ", days
+
+# Printing all month inlduing in variable which are till Aug.
+print "Here are the months: ", months
+
+# Printing a paragraph as string with two doubles quotes.
+print """
+There's something going on here.
+With the threee double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
