b543e37e0e9ff05c157cd2aa31c49d381cf67202
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-13 17:23:21 +0530

    added learn python hard way exercise 7: More Printing

---
+++ learn-python-hard-way-exercies/ex7.py
@@ -0,0 +1,57 @@
+# This is Exercise 7 : More Printing 
+
+# Printing a sentence
+print "Mary had a little lamb"
+
+# Printing another sentence using formatting
+print "Its fleece was white as %s." % 'snow'
+
+# Printing a sentence again
+print "And everywhere that Mary went."
+
+# Printing "." 10 times 
+print "." * 10 
+
+# A variable storing "C"
+end1 = "C"
+
+# A variable storing "h"
+end2 = "h"
+
+# A variable storing "e"
+end3 = "e"
+
+# A variable storing "e"
+end4 = "e"
+
+# a variable storing "s"
+end5 = "s"
+
+# a variable storing "e"
+end6 = "e"
+
+# a variable storing "B"
+end7 = "B"
+
+# a variable storing "u"
+end8 = "u"
+
+# a variable storing "r"
+end9 = "r"
+
+# a variable storing "g"
+end10 = "g"
+
+# a variables storing "e"
+end11 = "e"
+
+# a variable storing "r"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happnes
+
+# Print fisrt six variables using "+" concatnation and comma at the end.
+print end1 + end2 + end3 + end4 + end5 + end6,
+
+# Print after six variables 
+print end7 + end8 + end9 + end10 + end11 + end12
