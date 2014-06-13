38370be60c82987f840dbe96157b2cba489f2fcd
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-13 13:04:15 +0530

    learn python hard way exercise 2 completed

---
+++ learn-python-hard-way-exercies/ex2.py
@@ -0,0 +1,9 @@
+# A commment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the commment after is ignored
+
+# You can also use a commment to "disable" or commment out piece of code
+# print "This won't run."
+
+print "This will run."
