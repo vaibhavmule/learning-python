dae38db21a9211bc102c1d77773d3615378012d3
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-13 18:26:20 +0530

    added learn python hard way exercise 10

---
+++ learn-python-hard-way-exercies/ex10.py
@@ -0,0 +1,30 @@
+# This is Exercise 10: What was that? (It is talking about last exercise )
+
+# A variable storing a string and using "\t" for tab space.
+tabby_cat = "\tI'm tabbed in."
+
+# A variable storing a string and using "\n" for new line
+persian_cat = "I'm split\non a line."
+
+# A variable storing a string and using "\\"
+backslash_cat = "I'm \\ a \\ cat."
+
+# A variable storing a string including three double quotes and using "\t" for tabs space and "\n" for a line.
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# Printing a sentence 
+print tabby_cat
+
+# Printing another sentence which includes "\n"
+print persian_cat
+
+# Printing another sentence which includes "\\"
+print backslash_cat 
+
+# Printing another sentence which includes three double qoutes, \t and \n.
+print fat_cat
