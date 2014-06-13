07d60317ae603218ec19b40ec093e7706a8bf1a5
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-13 16:32:55 +0530

    added learning python hard way exercise 5

---
+++ learn-python-hard-way-exercies/ex5.py
@@ -0,0 +1,45 @@
+# This is Exercise 5 : More Variables and Printing
+
+# Storing Name in Variables.
+name = 'Vaibhav Mule'
+
+# Storing Age in Variables.
+age = 19
+
+# Storing Height in Variable. 
+height = 64 # inches
+
+# Storing weight in Variable.
+weight = 121 #lbs
+
+# Storing eyes color in Variable.
+eyes = 'Black'
+
+# Storing teeth color in Variable.
+teeth = 'White'
+
+# Storing hair color in Variable.
+hair = 'Black'
+
+# Print 'name' variable with sentence.
+print "Let's talk about %s." % name
+
+# Print 'age' variable with sentence.
+print "He's %d inches tall." % height
+
+# Print 'weight' variable with sentence.
+print "He's %d pounds heavy." % weight
+
+# Print some statement about weight
+print "Actually that's not too heavy."
+
+# Print color of eyes and hair.
+print "He's got %s eyes and %s hair." %(eyes, hair)
+
+# Print teeth color and some sentence.
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+
+# Print and calculate age + height + weight
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
