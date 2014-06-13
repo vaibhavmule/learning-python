f46784d98866d219caedf8af3cfb28d86b52245d
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-13 19:46:04 +0530

    added learn python hard exercise 13

---
+++ learn-python-hard-way-exercies/ex12.py
@@ -0,0 +1,14 @@
+# This is Exercise 12: Promptig People
+
+# Asking person's age and storing in a variable
+age = raw_input("How old are you?")
+
+# Asking person's height and storing in a variable
+height = raw_input("How tall are you?")
+
+# Asking for weight and storing it in a variable
+weight = raw_input("How much do you weigh?")
+
+# Printing a sentence with inputs using formatting
+print "So, you're %r old. %r tall and %r heavy." % (
+	age, height, weight)
---
+++ learn-python-hard-way-exercies/ex13.py
@@ -0,0 +1,20 @@
+# This is Exercise 13: Parameters, Unpacking, Variables
+
+# Import sys modules and argv is an argument variable
+# which holds arguments that you pass on Pyhton script
+from sys import argv
+
+# Unpacking and assing argv values to Variables
+script, first, second, third = argv
+
+# Prining a sentence with variable
+print "The script is called:", script
+
+# Prining another sentence with variable
+print "Your first variable is:", first
+
+# Printing another sentence with variable
+print "Your second variable is:", second
+
+# Printing another sentence with variable
+print "Your third variable is:", third
