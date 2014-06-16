27f643c60ea3c6545ad098afb9ac510462891e4f
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-16 10:58:26 +0530

    added learn python hard way exercise 29

---
+++ learn-python-hard-way-exercies/ex29.py
@@ -0,0 +1,35 @@
+# this is exercise 29: What If
+
+# create a variable storing how many people are there
+people = 20
+cats = 30 # a variable storing how many cats are there
+dogs = 15 # a variable storing how many dogs are there
+
+# cheking if people are less than cats
+if people < cats:
+	print "Too many cats! The world is doomed!"
+
+# cheking if people are greater than cats
+if people > cats:
+	print "Not many cats! The world is saved"
+
+# checking if people are less than dogs
+if people < dogs:
+	print "The world is drooled on!"
+
+# chking if people are greater than dogs
+if people > dogs:
+	print "The world is dry!"
+
+# 5 new dogs are added
+dogs += 5
+
+# checking if people are greater than or equal to dogs
+if people >= dogs:
+	print "People are greater than or equal to dogs"
+
+# checking if people are less than or equal to dogs
+if people <= dogs:
+	print "People are less than or equal to dogs"
+
+# checking if people and dogs are equal
