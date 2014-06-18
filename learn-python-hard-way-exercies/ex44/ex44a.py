543eb924a0f88335d058bc62168671810cfaceeb
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-18 23:18:18 +0530

    added learn python hard way exercise: 44

---
+++ learn-python-hard-way-exercies/ex44/ex44a.py
@@ -0,0 +1,15 @@
+# this is exercise 44: Inheritance vs Composition
+
+class Parent(object):
+
+	def implicit(self):
+		print "PARENT implicit()"
+
+class Child(Parent):
+	pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
---
+++ learn-python-hard-way-exercies/ex44/ex44c.py
@@ -0,0 +1,19 @@
+# this is exercise 44 C: Alter Before or After
+
+class Parent(object):
+
+	def altered(self):
+		print "PARENT altered()"
+
+class Child(Parent):
+
+	def altered(self):
+		print "CHILD, BEFORE PARENT altered()"
+		super(Child, self).altered()
+		print "CHILD, AFTER PARENT altered()"
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
