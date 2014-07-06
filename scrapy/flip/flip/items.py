bfcd97f4f085a77eab3a6a90053e18d4968c604e
vaibhavmule <vaibhavmule135@gmail.com>
2014-07-07 00:09:10 +0530

    Weekly commit removed files and folders..

--- scrapy/flip/flip/items.py
+++
@@ -1,8 +0,0 @@
-from scrapy.item import Item, Field
-from scrapy.contrib.loader import XPathItemLoader
-from scrapy.contrib.loader.processor import TakeFirst, MapCompose, Join,Compose
-
-class FlipkartItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    productName=Field()
