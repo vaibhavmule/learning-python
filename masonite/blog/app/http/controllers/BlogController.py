75cd0d98e632cdc35ea50fa04f10970205c21c22
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-09-10 16:20:53 +0530

    add masonite

---
+++ masonite/blog/app/http/controllers/BlogController.py
@@ -0,0 +1,19 @@
+''' A Module Description '''
+from app.Post import Post
+
+
+class BlogController:
+    ''' Class Docstring Description '''
+
+    def show(self):
+        return view('blog')
+
+    def store(self):
+        Post.create(
+            title=request().input('title'),
+            body=request().input('body'),
+            author_id=request().user().id
+        )
+
+        return 'Post created'
+
---
+++ masonite/blog/app/http/controllers/PostController.py
@@ -0,0 +1,43 @@
+''' A Module Description '''
+from app.Post import Post
+
+class PostController:
+    ''' Class Docstring Description '''
+
+    def show(self):
+        posts = Post.all()
+
+        return view('posts', {'posts': posts})
+
+
+    def single(self):
+        post = Post.find(request().param('id'))
+
+        print(request().param('id'))
+
+        return view('single', {'post': post})
+
+
+    def update(self):
+        post = Post.find(request().param('id'))
+
+        return view('update', {'post': post})
+
+
+    def store(self):
+        post = Post.find(request().param('id'))
+
+        post.title = request().input('title')
+        post.body = request().input('body')
+
+        post.save()
+
+        return 'post updated'
+
+
+    def delete(self):
+        post = Post.find(request().param('id'))
+
+        post.delete()
+
+        return 'post deleted'
